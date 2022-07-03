687cb1736374e750198c9185473d6f2f12123c24
Jhenifer <jhenifer.msantos@gmail.com>
2022-07-03 11:06:57 -0300

    04. Criando, deletando e editando com Gorm

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -23,13 +23,52 @@ func GetAll(w http.ResponseWriter, r *http.Request) {
 }
 
 func Get(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	id := vars["id"]
+	id := getIdFromUrl(r)
 
 	var personalidade models.Personalidade
 
 	database.DB.First(&personalidade, id)
 
 	json.NewEncoder(w).Encode(personalidade)
+}
+
+func getIdFromUrl(r *http.Request) string {
+	vars := mux.Vars(r)
+
+	return vars["id"]
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var novaPersonalidade models.Personalidade
+
+	json.NewDecoder(r.Body).Decode(&novaPersonalidade)
+
+	database.DB.Create(&novaPersonalidade)
+
+	json.NewEncoder(w).Encode(novaPersonalidade)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	id := getIdFromUrl(r)
+
+	var personalidade models.Personalidade
+
+	database.DB.Delete(&personalidade, id)
+
+	json.NewEncoder(w).Encode(personalidade)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+
+	id := getIdFromUrl(r)
+
+	var personalidadeUpdate models.Personalidade
+
+	database.DB.Find(&personalidadeUpdate, id)
+
+	json.NewDecoder(r.Body).Decode(&personalidadeUpdate)
+
+	database.DB.Save(&personalidadeUpdate)
+
+	json.NewEncoder(w).Encode(personalidadeUpdate)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -13,5 +13,8 @@ func HandleRequest() {
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.GetAll).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.Get).Methods("Get")
+	r.HandleFunc("/api/personalidades/{id}", controllers.Delete).Methods("Delete")
+	r.HandleFunc("/api/personalidades/{id}", controllers.Update).Methods("Put")
+	r.HandleFunc("/api/personalidades", controllers.Create).Methods("Post")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
